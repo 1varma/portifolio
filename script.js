dfb8ff01aa028d615e6a574e20a48ccc4f1e0557
Ashish Varma Juttu <varmaashish53@gmail.com>
2024-05-17 18:50:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let scrollableElement = document.getElementById("innerProjects");
+
+scrollableElement.addEventListener("mouseenter", function() {
+  this.style.overflowX = "hidden";
+});
+
+scrollableElement.addEventListener("mouseleave", function() {
+  this.style.overflowX = "auto";
+});
+
+scrollableElement.addEventListener("wheel", function(event) {
+  if (this.clientWidth < this.scrollWidth) {
+    this.scrollLeft += event.deltaY;
+    event.preventDefault();
+  }
+});
